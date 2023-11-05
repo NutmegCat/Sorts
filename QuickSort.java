7522aa39ea00a06121f1501fd6090657d9d256c8
NutmegCat <aaronprince.anu@student.tdsb.on.ca>
2023-11-04 20:34:14 -0400

    format + final

--- QuickSort.java
+++ QuickSort.java
@@ -3,7 +3,7 @@ import java.util.Random;
 public class QuickSort {
     public static void main(String[] args) {
         Random rand = new Random();
-        int [] numbers = new int[10];
+        int[] numbers = new int[10];
 
         for (int i = 0; i < numbers.length; i++) {
             numbers[i] = rand.nextInt(100);
@@ -15,27 +15,27 @@ public class QuickSort {
         quicksort(numbers, 0, numbers.length - 1);
 
         System.out.println("\nAfter:");
-        printArray(numbers);        
+        printArray(numbers);
     }
 
     private static void quicksort(int[] array, int lowIndex, int highIndex) {
-        
+
         if (lowIndex >= highIndex) {
             return;
         }
 
         int pivot = array[highIndex];
-        
+
         int leftPointer = lowIndex;
         int rightPointer = highIndex;
 
         while (leftPointer < rightPointer) {
             while (array[leftPointer] <= pivot && leftPointer < rightPointer) {
                 leftPointer++;
-                
+
             }
 
-            while (array [rightPointer] >= pivot && leftPointer < rightPointer) {
+            while (array[rightPointer] >= pivot && leftPointer < rightPointer) {
                 rightPointer--;
             }
 
@@ -49,13 +49,13 @@ public class QuickSort {
 
     }
 
-    private static void swap (int[] array, int index1, int index2) {
+    private static void swap(int[] array, int index1, int index2) {
         int temp = array[index1];
         array[index1] = array[index2];
         array[index2] = temp;
     }
 
-    private static void printArray (int[] numbers) {
+    private static void printArray(int[] numbers) {
         for (int i = 0; i < numbers.length; i++) {
             System.out.println(numbers[i]);
         }
