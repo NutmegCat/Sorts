1b39a5ad583b29ecdf6c8e90cfc21347ae9c50b7
NutmegCat <aaronprince.anu@student.tdsb.on.ca>
2023-11-08 20:35:06 -0500

    insertionSort method

---
+++ InsertionSort.java
@@ -0,0 +1,34 @@
+import java.util.Random;
+
+public class InsertionSort{
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int[] numbers = new int[10];
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = rand.nextInt(100);
+        }
+
+        System.out.println("Before: ");
+        printArray(numbers);
+
+        insertionSort(numbers);
+
+        System.out.println("\nAfter: ");
+        printArray(numbers);
+    }
+
+    private static void insertionSort (int[] inputArray) {
+        for (int i = 1; i < inputArray.length; i++) {
+            int currentValue = inputArray[i];
+
+            int j = i - 1;
+            
+            while (j >= 0 && inputArray[j] > currentValue) {
+                inputArray[j + 1] = inputArray[j];
+                j--;
+            }
+            inputArray[j + 1] = currentValue;
+        }
+    }
+}
