9b0b4d8a4fda0bbcaf6d6860b9b95ee67af75f3e
NutmegCat <aaronprince.anu@student.tdsb.on.ca>
2023-11-27 16:21:55 -0500

    selectionSort method

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Random;
 
 class SelectionSort {
@@ -7,6 +8,23 @@ class SelectionSort {
 
         for (int i = 0; i < numbers.length; i++) {
             numbers[i] = random.nextInt(100000);
+        }
+        System.out.println(Arrays.toString(numbers));
+        selectionSort(numbers);
+    }
+
+    private static void selectionSort(int[] numbers) {
+        int length = numbers.length;
+
+        for (int i = 0; i < length - 1; i++) {
+            int min = numbers[i];
+            int indexOfMin = i;
+            for (int j = i +1; j < length; j++) {
+                if (numbers[j] < min) {
+                    min = numbers[j];
+                    indexOfMin = j;
+                }
+            }
         }
     }
 }
