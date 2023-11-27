53dbd2f37e67c2036154b141ade599fb1dacde4c
NutmegCat <aaronprince.anu@student.tdsb.on.ca>
2023-11-27 16:18:23 -0500

    first commit + main

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,3 +1,12 @@
+import java.util.Random;
+
 class SelectionSort {
-    
+    public static void main(String[] args) {
+        int[] numbers = new int[10];
+        Random random = new Random();
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = random.nextInt(100000);
+        }
+    }
 }
